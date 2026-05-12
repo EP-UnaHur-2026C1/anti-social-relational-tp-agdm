40078debdd44e633a6a21636d1b9541a447a2b2d
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2026-05-11 21:14:06 -0300

    add dependencies

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to UnaHur - Anti-Social net!");
+});
+
+app.listen(port, () => {
+  console.log(`UnaHur app listening at http://localhost:${port}`);
+});
+
 console.log("UnaHur - Anti-Social net");
