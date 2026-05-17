57057ab6216388ad02603913ebfab737b3abf55f
unknown <andres.martinez@estudiantes.unahur.edu.ar>
2026-05-17 19:16:36 -0300

    Inicio de Sequelize: Modelos, migraciones y armado de carpetas

---
+++ models/tag.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsToMany(models.Post, { through: 'PostTags', foreignKey: 'tagId', otherKey: 'postId' });
+    }
+  }
+  Tag.init({
+    nombre: { type: DataTypes.STRING, unique: true, allowNull: false }
+  }, {
+    sequelize,
+    modelName: 'Tag',
+  });
+  return Tag;
+};
