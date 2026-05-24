82f4e496a062be5f4a27ce4b308396b6fedf8af6
DarkoDv <damianhbn23@gmail.com>
2026-05-24 12:34:29 -0300

    arreglo de errores

--- middlewares/validarUsuario.middleware.js
+++ middlewares/validarUsuario.middleware.js
@@ -1,6 +1,6 @@
 
 const validarUsuario = (schema) => (req, res, next) => {
-  const { error } = schema.validarUsuario(req.body, { abortEarly: false });
+  const { error } = schema.validate(req.body, { abortEarly: false });
   if (error) {
     const messages = error.details.map((d) => d.message);
     return res.status(400).json({ errors: messages });
