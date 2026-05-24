e0e47a2ae84e37f43771df550731c1340d03bb95
DarkoDv <damianhbn23@gmail.com>
2026-05-24 12:11:19 -0300

    usuarioterminado

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const userRouter = require('./routes/user.routes');
+app.use(express.json());
+app.use('/users', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to UnaHur - Anti-Social net!');
+});
+
+app.listen(port, () => {
+  console.log(`UnaHur app listening at http://localhost:${port}`);
+});
---
+++ schemas/user.schema.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const validarUsuario = require('../middlewares/validarUsuario.middleware');
+
+
+const crearUsuarioSchema = Joi.object({
+  nickName: Joi.string().min(3).max(30).required().messages({
+    'string.empty': 'El nickName no puede estar vacío',
+    'any.required': 'El nickName es obligatorio'
+  }),
+  email: Joi.string().email().required().messages({
+    'string.email': 'El formato del email no es válido',
+    'any.required': 'El email es obligatorio'
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.min': 'La contraseña debe tener al menos 6 caracteres',
+    'any.required': 'La contraseña es obligatoria'
+  })
+});
+
+const actualizarUsuarioSchema = Joi.object({
+  email: Joi.string().email().optional(),
+  password: Joi.string().min(6).optional()
+}).min(1).messages({
+  'object.min': 'Debes enviar al menos un campo (email o password) para actualizar'
+});
+
+const followSchema = Joi.object({
+  seguidor: Joi.string().required(),
+  seguido: Joi.string().required()
+});
+
+
+module.exports = {
+  validarCreacion: validarUsuario(crearUsuarioSchema),
+  validarActualizacion: validarUsuario(actualizarUsuarioSchema),
+  validarFollow: validarUsuario(followSchema)
+};
